b9b414ed1fba7d08c8e7e9b8405a3f189c487e3b
guitarbum722 <johnkenneth.moore@gmail.com>
2017-05-10 20:29:00 -0700

    Output the implemented methods for the provided types

--- commands/implement.go
+++ commands/implement.go
@@ -1,14 +1,10 @@
 package commands
 
 import (
-	_ "bufio"
 	"fmt"
 	"github.com/mitchellh/cli"
-	_ "log"
-	_ "os"
-	_ "path/filepath"
-	_ "regexp"
-	_ "strings"
+	"log"
+	"strings"
 )
 
 // Implement provides commands to provide the skeletal implementation of the specified interface
@@ -28,6 +24,34 @@ func (i *Implement) Run(args []string) int {
 		fmt.Println(i.Help())
 		return 0
 	}
+
+	ifaces, err := searchInterfaces(false)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for i := range ifaces {
+		ifaces[i].methods = append(ifaces[i].methods, tempMethods[ifaces[i].name]...)
+	}
+	methods, found := Contains(ifaces, args[0])
+
+	if !found {
+		fmt.Printf("This package does not contain the specified interface: %s\n", args[0])
+		return 0
+	}
+
+	funcTemplate := `
+func (%s %s) %s {
+
+}
+
+`
+	// TODO(guitarbum722) provide a flag so that the receiver can be a pointer to the type 2017-05-10T18:30 4
+	for _, arg := range args[1:] {
+		for i := range methods {
+			fmt.Printf(funcTemplate, strings.ToLower(string(arg[0])), arg, methods[i])
+		}
+	}
+
 	return 0
 }
 
